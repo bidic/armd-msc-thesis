127c264277533d157e11fcf4ca2550dd834e38b2
NowackiM <NowackiM@7c5b2a9a-bee7-11de-9616-7b1d4728551e>
2010-05-24 11:23:46 +0000

    Nowa wersja API Javowego

--- DarkExplorerJavaAPI/src/pl/edu/agh/fis/DarkExplorer/BTConnection.java
+++ DarkExplorerJavaAPI/src/pl/edu/agh/fis/DarkExplorer/BTConnection.java
@@ -10,7 +10,9 @@
 package pl.edu.agh.fis.DarkExplorer;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Calendar;
 
 import javax.microedition.io.Connector;
 import javax.microedition.io.StreamConnection;
@@ -21,6 +23,7 @@ public class BTConnection {
 	
 	private final String url = "btspp://00126F0437A2:1";
 	private OutputStream outstream = null;
+	private InputStream instream = null;
 	
 	private BTConnection()
 	{
@@ -33,11 +36,9 @@ public class BTConnection {
 	}
 
 	public void createNewConnection() throws IOException {
-		
-		System.out.println("New connection");
 		if (stream != null)
 			stream.close();
-		stream = (StreamConnection) Connector.open(url,Connector.WRITE,true);
+		stream = (StreamConnection) Connector.open(url,Connector.READ_WRITE,true);
 		try
 		{
 			Thread.sleep(2000);
@@ -75,6 +76,37 @@ public class BTConnection {
 			outstream.flush();
 		}
 
+	}
+	
+	public void readBtData(byte[] data, int currOffset, int len) throws IOException
+	{
+		if(instream == null)
+			instream = stream.openInputStream();
+
+		try	//give some time for DarkExplorer to make a picture
+		{
+				Thread.sleep(10);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	    long startTime = Calendar.getInstance().getTime().getTime();
+	    long currTime = startTime;
+	    int startOffset = currOffset;
+	    int summaryDataRead = 0;
+
+		do
+		{
+			summaryDataRead = currOffset - startOffset;
+			int singleDataRead = instream.read(data, currOffset, len - summaryDataRead );
+			currOffset += singleDataRead;
+			currTime = Calendar.getInstance().getTime().getTime();
+		}
+		while(summaryDataRead < len && currTime < startTime + 2000);
+		
+		if(summaryDataRead < len)
+			throw new IOException("Error in bluetooth communication.");
 	}
 
 	public void closeOpenConnection() {
