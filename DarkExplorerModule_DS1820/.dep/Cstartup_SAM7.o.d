226a867cf8b042f0f4cec6ee1ea678d003fdabb4
lukashanusiak <lukashanusiak@gmail.com>
2010-07-21 15:47:25 +0000

    upgrade do at91lib modułu ds1820

---
+++ DarkExplorerModule_DS1820/.dep/Cstartup_SAM7.o.d
@@ -0,0 +1,8 @@
+Cstartup_SAM7.o: Cstartup_SAM7.c Board.h AT91SAM7S256.h \
+  lib_AT91SAM7S256.h
+
+Board.h:
+
+AT91SAM7S256.h:
+
+lib_AT91SAM7S256.h:
---
+++ DarkExplorerModule_DS1820/.dep/main.o.d
@@ -0,0 +1,16 @@
+main.o: main.c dallas_ds1820.h Board.h AT91SAM7S256.h lib_AT91SAM7S256.h \
+  pio.h lcd.c delay.h
+
+dallas_ds1820.h:
+
+Board.h:
+
+AT91SAM7S256.h:
+
+lib_AT91SAM7S256.h:
+
+pio.h:
+
+lcd.c:
+
+delay.h:
